b370921ec91fe97c92851f18569ddc59ba24a4de
Diogo Costa <dcosta.mangualde@gmail.com>
2018-12-23 18:02:39 +0000

    Responsive css tweaks/fixes

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sslify import SSLify
-import requests, sys
+import requests, sys, datetime
 
 app = Flask(__name__)
 sslify = SSLify(app, subdomains=True)
@@ -10,4 +10,7 @@ def index():
     r = requests.get("https://api.github.com/users/DiogoTheCoder/repos")
     if r:
         data = r.json()
-    return render_template('index.html', projectNum=len(data))
+
+
+    years = datetime.date.today().year - datetime.date(2012, 1, 1).year
+    return render_template('index.html', projectNum=len(data), yearsOfXp=years)
